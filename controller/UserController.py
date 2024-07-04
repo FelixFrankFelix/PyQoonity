56f9f28108c9d597cb1552b16e2b65ce3c1614de
FelixFrankFelix <frankozie4life@gmail.com>
2024-07-04 13:35:43 +0100

    udate to update

--- controller/UserController.py
+++ controller/UserController.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List,Union
-from model.request import CrudRequest
-from model.response import CrudResponse
-from repository.database import CrudRepository
+from model.request import UserRequest
+from model.response import UserResponse
+from repository.database import UserRepository
 from db import SessionLocal, engine
 from utility.exceptions import ResponseConstant
-from service import CrudService
+from service import UserService
 
 app = FastAPI()
 
@@ -18,43 +18,43 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/create-user/", response_model=CrudResponse.ResponseUnionUser)
-def create_user_controller(user: CrudRequest.UserCreate, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_email(db, user.user_email)
+@app.post("/create-user/", response_model=UserResponse.ResponseUnionUser)
+def create_user_controller(user: UserRequest.UserCreate, db: Session = Depends(get_db)):
+    db_user = UserRepository.get_user_by_email(db, user.user_email)
     if not db_user:
-        db_user = CrudRepository.create_user(db=db, user=user)
-        return CrudService.create_user_service(db_user)
-    return CrudService.create_user_service(None)
+        db_user = UserRepository.create_user(db=db, user=user)
+        return UserService.create_user_service(db_user)
+    return UserService.create_user_service(None)
 
-@app.get("/read-users/", response_model= CrudResponse.ResponseUnionUsers)
+@app.get("/read-users/", response_model= UserResponse.ResponseUnionUsers)
 def read_users_controller(db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_users(db)
-    return CrudService.read_users_service(db_user)
+    db_user = UserRepository.get_users(db)
+    return UserService.read_users_service(db_user)
 
 
-@app.get("/read-user-by-id/{user_id}", response_model=CrudResponse.ResponseUnionUser)
+@app.get("/read-user-by-id/{user_id}", response_model=UserResponse.ResponseUnionUser)
 def read_user_by_id_controller(user_id: int, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_id(db, user_id=user_id)
-    return CrudService.read_user_by_id_service(db_user)
+    db_user = UserRepository.get_user_by_id(db, user_id=user_id)
+    return UserService.read_user_by_id_service(db_user)
 
-@app.get("/read-user-by-email/{user_email}", response_model=CrudResponse.ResponseUnionUser)
+@app.get("/read-user-by-email/{user_email}", response_model=UserResponse.ResponseUnionUser)
 def read_user_by_email_controller(user_email: str, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_email(db, user_email=user_email)
-    return CrudService.read_user_by_email_service(db_user)
+    db_user = UserRepository.get_user_by_email(db, user_email=user_email)
+    return UserService.read_user_by_email_service(db_user)
 
-@app.get("/read-user-by-name/{user_name}", response_model=CrudResponse.ResponseUnionUsers)
+@app.get("/read-user-by-name/{user_name}", response_model=UserResponse.ResponseUnionUsers)
 def read_user_by_name(user_name: str, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_name(db, user_name=user_name)
-    return CrudService.read_user_by_name_service(db_user)
+    db_user = UserRepository.get_user_by_name(db, user_name=user_name)
+    return UserService.read_user_by_name_service(db_user)
 
 
-@app.post("/update-user/{user_id}", response_model=CrudResponse.ResponseUnionUser)
-def udate_user_controller(user_id: int, user: CrudRequest.UserUpdate, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_id(db, user_id=user_id)
-    return CrudService.update_user_service(db_user)
+@app.post("/update-user/{user_id}", response_model=UserResponse.ResponseUnionUser)
+def update_user_controller(user_id: int, user: UserRequest.UserUpdate, db: Session = Depends(get_db)):
+    db_user = UserRepository.get_user_by_id(db, user_id=user_id)
+    return UserService.update_user_service(db_user)
 
 
-@app.post("/delete-user/{user_id}", response_model=CrudResponse.ResponseUnionUser)
+@app.post("/delete-user/{user_id}", response_model=UserResponse.ResponseUnionUser)
 def delete_user_controller(user_id: int, db: Session = Depends(get_db)):
-    db_user = CrudRepository.get_user_by_id(db, user_id=user_id)
-    return CrudService.delete_user_service(db_user)
+    db_user = UserRepository.get_user_by_id(db, user_id=user_id)
+    return UserService.delete_user_service(db_user)
